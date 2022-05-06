2c2aa60c750649c22c522ea60fbb384bc57e2215
root <root@ip-172-31-9-220.ap-south-1.compute.internal>
2022-05-06 07:17:05 +0000

    om first commit

---
+++ om.java
@@ -0,0 +1,10 @@
+try (BufferedInputStream in = new BufferedInputStream(new URL(FILE_URL).openStream());
+  FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+    byte dataBuffer[] = new byte[1024];
+    int bytesRead;
+    while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+        fileOutputStream.write(dataBuffer, 0, bytesRead);
+    }
+} catch (IOException e) {
+    // handle exception
+}
